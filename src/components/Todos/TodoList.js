46f93a6104b8a691a7094af74933ccda746768a0
Kate <ekatchelysheva@gmail.com>
2023-05-10 15:42:33 +0300

    create first version app: add components

---
+++ src/components/Todos/TodoList.js
@@ -0,0 +1,12 @@
+import Todo from './Todo'
+import styles from './TodoList.module.css'
+
+function TodoList ({todos}){
+    return (
+        <div className={styles.todoListContainer}>
+    {todos.map((todo,index) => <Todo key={index} todo ={todo} />)}
+    </div>
+    )
+}
+
+export default TodoList
