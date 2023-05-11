a306aad7af8eae2327eff9b3983ee7049a1c5e05
Kate <ekatchelysheva@gmail.com>
2023-05-11 16:03:06 +0300

    add icon

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,8 +1,10 @@
+import {RiArchiveLine} from 'react-icons/ri'
 import styles from './Todo.module.css'
 
 function Todo({todo, index, deleteTodo}) {
     return (
 <div className={styles.todo} onDoubleClick={() => deleteTodo(index)}>
+    <RiArchiveLine className={styles.todoIcon} />
     <div className= {styles.todoText}>{todo}</div>
 </div>
     ) 
